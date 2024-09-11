f93212cdcff0e5159d63e444fd4e0c10d8ad8bf0
omercanyenigun <77763663+omercanyenigun@users.noreply.github.com>
2024-09-11 16:22:12 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,69 +1,73 @@
 #!/bin/bash
 
+# Renk kodları
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color
+
 # Sistem güncellemeleri
-echo "Sistem güncellemeleri yapılıyor..."
+echo -e "${ORANGE}Sistem güncellemeleri yapılıyor...${NC}"
 sudo apt update && sudo apt upgrade -y
 
 # Gerekli paketlerin yüklenmesi
-echo "Gerekli paketler yükleniyor..."
+echo -e "${ORANGE}Gerekli paketler yükleniyor...${NC}"
 sudo apt install make clang pkg-config libssl-dev build-essential git screen protobuf-compiler -y
 
 # Docker kurulumu
-echo "Docker kurulumu yapılıyor..."
+echo -e "${ORANGE}Docker kurulumu yapılıyor...${NC}"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 # Node.js kurulumu
-echo "Node.js kurulumu yapılıyor..."
+echo -e "${ORANGE}Node.js kurulumu yapılıyor...${NC}"
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 
 # Docker için gerekli ek paketler yükleniyor
-echo "Docker ek paketleri yükleniyor..."
+echo -e "${ORANGE}Docker ek paketleri yükleniyor...${NC}"
 sudo apt install docker.io -y
 
 # Docker Compose kurulumu
-echo "Docker Compose kurulumu yapılıyor..."
+echo -e "${ORANGE}Docker Compose kurulumu yapılıyor...${NC}"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
 # Node.js ve npm kurulumu
-echo "Node.js ve npm kurulumu yapılıyor..."
+echo -e "${ORANGE}Node.js ve npm kurulumu yapılıyor...${NC}"
 sudo apt-get install -y nodejs
 sudo apt install -y nodejs npm
 nodejs --version
 
 # Yarn kurulumu
-echo "Yarn kurulumu yapılıyor..."
+echo -e "${ORANGE}Yarn kurulumu yapılıyor...${NC}"
 sudo npm install -g yarn
 yarn install
 
 # CAT Protocol projesinin klonlanması
-echo "CAT Protocol projesi klonlanıyor..."
+echo -e "${ORANGE}CAT Protocol projesi klonlanıyor...${NC}"
 git clone https://github.com/CATProtocol/cat-token-box.git
 
 # Proje dizinine geçiş ve kurulumu
 cd cat-token-box
-echo "Proje kurulumu yapılıyor..."
+echo -e "${ORANGE}Proje kurulumu yapılıyor...${NC}"
 yarn install && yarn build
 
 # Docker dosyaları için izinlerin ayarlanması
 cd packages/tracker
-echo "Docker dosyaları için izinler ayarlanıyor..."
+echo -e "${ORANGE}Docker dosyaları için izinler ayarlanıyor...${NC}"
 sudo chmod 777 docker/data
 sudo chmod 777 docker/pgdata
 
 # Docker Compose çalıştırılması
-echo "Docker Compose başlatılıyor..."
+echo -e "${ORANGE}Docker Compose başlatılıyor...${NC}"
 docker-compose up -d
 
 # Ana dizine dönüp Docker image oluşturulması
 cd ../../
-echo "Docker image oluşturuluyor..."
+echo -e "${ORANGE}Docker image oluşturuluyor...${NC}"
 docker build -t tracker:latest .
 
 # Docker container çalıştırılması
-echo "Tracker container başlatılıyor..."
+echo -e "${ORANGE}Tracker container başlatılıyor...${NC}"
 docker run -d \
     --name tracker \
     --add-host="host.docker.internal:host-gateway" \
@@ -73,5 +77,5 @@ docker run -d \
     tracker:latest
 
 # Synced Kontrol
-echo "Synced kontrol yapılıyor..."
+echo -e "${ORANGE}Synced kontrol yapılıyor...${NC}"
 curl http://127.0.0.1:3000/api
